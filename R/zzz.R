16ad2e4314671d0c04c7f28b1901636def278d6e
jcizel <jcizel@gmail.com>
2014-11-28 22:16:19 +0100

    bug fixes

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(...){
-    cl <- parallel:::makeCluster(8, outfile = "./log/l1.log")
+    cl <- parallel:::makeCluster(8)
     doParallel:::registerDoParallel(cl)
 
     options(java.parameters = "-Xmx4000m")
